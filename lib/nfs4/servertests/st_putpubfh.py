aec19827169767020dbcea04302b8766eba780a8
Fred Isaman <iisaman@citi.umich.edu>
2009-03-11 09:50:10 -0400

    TESTS: Modify PUTPUBFH tests to be more strict
    Per recent discussion (see thread started by
    <http://linux-nfs.org/pipermail/nfsv4/2009-March/009995.html>),
    NFS4ERR_NOTSUPP is not an allowable response to PUTPUBFH.  Modify PUB1
    accordingly.
    
    While we are at it, add PUB2, which tests the RFC 3530 statement that
    the public and root file handles "should" be equivalent
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>

--- lib/nfs4/servertests/st_putpubfh.py
+++ lib/nfs4/servertests/st_putpubfh.py
@@ -1,5 +1,5 @@
 from nfs4.nfs4_const import *
-from environment import checklist
+from environment import check
 
 def testSupported(t, env):
     """Do a simple PUTPUBFH
@@ -11,6 +11,31 @@ def testSupported(t, env):
     c = env.c1
     ops = [c.putpubfh_op()]
     res = c.compound(ops)
-    checklist(res, [NFS4_OK, NFS4ERR_NOTSUPP])
-    if res.status == NFS4ERR_NOTSUPP:
-        t.fail_support("PUTPUBFH not supported")
+    check(res)
+
+def testSameAsRoot(t, env):
+    """PUTPUBFH should return same as PUTROOTFH
+
+    Per RFC 2530:
+    The public filehandle and the root filehandle (represented by the
+    PUTROOTFH operation) should be equivalent
+
+    FLAGS: putpubfh all
+    DEPEND: PUB1
+    CODE: PUB2
+    """
+
+    c = env.c1
+    ops = [c.putpubfh_op(), c.getfh_op()]
+    res = c.compound(ops)
+    check(res)
+    pubfh = res.resarray[-1].switch.switch.object
+
+    ops = [c.putrootfh_op(), c.getfh_op()]
+    res = c.compound(ops)
+    check(res)
+    rootfh = res.resarray[-1].switch.switch.object
+
+    if rootfh != pubfh:
+        # This is not a failure, since is merely a "should", but will warn about
+        t.pass_warn('Spec says rootfh "should" equal pubfh')
