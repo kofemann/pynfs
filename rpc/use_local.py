0535d264a0c2c7f52f7415db91978b265e89dd36
Fred Isaman <iisaman@citi.umich.edu>
2009-09-29 17:55:26 -0400

    RPC: Work around Windows aversion to symlinks
    The symlink to use_local does not work in cygwin python
    under Windows Vista.  For the moment, just copy the file.
    
    While we are removing symlinks, the gssapi symlinks are not
    needed now that use_local is being used.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>

--- rpc/use_local.py
+++ rpc/use_local.py
@@ -1,1 +1,13 @@
-../nfs4.1/use_local.py
+import sys
+import os
+from os.path import join, split
+cwd = os.getcwd()
+if True or cwd not in sys.path:
+    head, tail = split(cwd)
+    dirs = [ join(head, "gssapi"),
+             join(head, "xdr"),
+             join(head, "ply"),
+             join(head), # rpc
+             cwd,
+             ]
+    sys.path[1:1] = dirs
