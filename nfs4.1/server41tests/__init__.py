02458748f4fbe7dc746dba6351464e852a8f4e65
J. Bruce Fields <bfields@redhat.com>
2010-10-20 12:39:19 -0400

    CLNT: Add trunking tests
    Note may want to move stuff from st_destroy_session to here as well?

--- nfs4.1/server41tests/__init__.py
+++ nfs4.1/server41tests/__init__.py
@@ -3,6 +3,7 @@ __all__ = ["st_exchange_id.py", # draft 21
            "st_create_session.py",
            "st_destroy_session.py",
            "st_sequence.py",
+	   "st_trunking.py",
            "st_open.py",
            "st_verify.py",
            "st_getdevicelist.py",
