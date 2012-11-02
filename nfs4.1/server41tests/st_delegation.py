78bea04866762b86cd63b4283afc2a52b0564181
J. Bruce Fields <bfields@redhat.com>
2012-11-02 11:58:35 -0400

    4.1 server tests: gather common test code into a helper
    There's a ludicrous amount of cut-n-paste here.
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- nfs4.1/server41tests/st_delegation.py
+++ nfs4.1/server41tests/st_delegation.py
@@ -8,12 +8,7 @@ from nfs4_type import creatverfattr, fattr4, stateid4, locker4, lock_owner4
 import nfs4_ops as op
 import threading
 
-def testReadDeleg(t, env):
-    """Test read delgation handout and return
-
-    FLAGS: open deleg all
-    CODE: DELEG1
-    """
+def _testDeleg(t, env, openaccess, want, breakaccess):
     recall = threading.Event()
     def pre_hook(arg, env):
         recall.stateid = arg.stateid # NOTE this must be done before set()
@@ -23,20 +18,18 @@ def testReadDeleg(t, env):
     sess1 = env.c1.new_client_session("%s_1" % env.testname(t))
     sess1.client.cb_pre_hook(OP_CB_RECALL, pre_hook)
     sess1.client.cb_post_hook(OP_CB_RECALL, post_hook)
-    res = create_file(sess1, env.testname(t),
-                      access=OPEN4_SHARE_ACCESS_READ |
-                      OPEN4_SHARE_ACCESS_WANT_READ_DELEG)
+    res = create_file(sess1, env.testname(t), access = openaccess | want)
     check(res)
     fh = res.resarray[-1].object
     deleg = res.resarray[-2].delegation
-    if deleg.delegation_type == OPEN_DELEGATE_NONE or deleg.delegation_type == OPEN_DELEGATE_NONE_EXT:
+    if    (deleg.delegation_type == OPEN_DELEGATE_NONE or
+           deleg.delegation_type == OPEN_DELEGATE_NONE_EXT):
         fail("Could not get delegation")
     sess2 = env.c1.new_client_session("%s_2" % env.testname(t))
     claim = open_claim4(CLAIM_NULL, env.testname(t))
     owner = open_owner4(0, "My Open Owner 2")
     how = openflag4(OPEN4_NOCREATE)
-    open_op = op.open(0, OPEN4_SHARE_ACCESS_BOTH, OPEN4_SHARE_DENY_NONE,
-                      owner, how, claim)
+    open_op = op.open(0, breakaccess, OPEN4_SHARE_DENY_NONE, owner, how, claim)
     slot = sess2.compound_async(env.home + [open_op])
     # Wait for recall, and return delegation
     recall.wait() # STUB - deal with timeout
@@ -48,6 +41,15 @@ def testReadDeleg(t, env):
     # Now get OPEN reply
     res = sess2.listen(slot)
     checklist(res, [NFS4_OK, NFS4ERR_DELAY])
+
+def testReadDeleg(t, env):
+    """Test read delgation handout and return
+
+    FLAGS: open deleg all
+    CODE: DELEG1
+    """
+    _testDeleg(t, env, OPEN4_SHARE_ACCESS_READ,
+        OPEN4_SHARE_ACCESS_WANT_READ_DELEG, OPEN4_SHARE_ACCESS_BOTH)
 
 def testWriteDeleg(t, env):
     """Test write delgation handout and return
@@ -55,40 +57,8 @@ def testWriteDeleg(t, env):
     FLAGS: open deleg all
     CODE: DELEG2
     """
-    recall = threading.Event()
-    def pre_hook(arg, env):
-        recall.stateid = arg.stateid # NOTE this must be done before set()
-        env.notify = recall.set # This is called after compound sent to queue
-    def post_hook(arg, env, res):
-        return res
-    sess1 = env.c1.new_client_session("%s_1" % env.testname(t))
-    sess1.client.cb_pre_hook(OP_CB_RECALL, pre_hook)
-    sess1.client.cb_post_hook(OP_CB_RECALL, post_hook)
-    res = create_file(sess1, env.testname(t),
-                      access=OPEN4_SHARE_ACCESS_BOTH |
-                      OPEN4_SHARE_ACCESS_WANT_WRITE_DELEG)
-    check(res)
-    fh = res.resarray[-1].object
-    deleg = res.resarray[-2].delegation
-    if deleg.delegation_type == OPEN_DELEGATE_NONE or deleg.delegation_type == OPEN_DELEGATE_NONE_EXT:
-        fail("Could not get delegation")
-    sess2 = env.c1.new_client_session("%s_2" % env.testname(t))
-    claim = open_claim4(CLAIM_NULL, env.testname(t))
-    owner = open_owner4(0, "My Open Owner 2")
-    how = openflag4(OPEN4_NOCREATE)
-    open_op = op.open(0, OPEN4_SHARE_ACCESS_READ, OPEN4_SHARE_DENY_NONE,
-                      owner, how, claim)
-    slot = sess2.compound_async(env.home + [open_op])
-    # Wait for recall, and return delegation
-    recall.wait() # STUB - deal with timeout
-    # Getting here means CB_RECALL reply is in the send queue.
-    # Give it a moment to actually be sent
-    env.sleep(1)
-    res = sess1.compound([op.putfh(fh), op.delegreturn(recall.stateid)])
-    check(res)
-    # Now get OPEN reply
-    res = sess2.listen(slot)
-    checklist(res, [NFS4_OK, NFS4ERR_DELAY])
+    _testDeleg(t, env, OPEN4_SHARE_ACCESS_WRITE,
+       OPEN4_SHARE_ACCESS_WANT_WRITE_DELEG, OPEN4_SHARE_ACCESS_READ)
 
 def testAnyDeleg(t, env):
     """Test any delgation handout and return
@@ -96,40 +66,8 @@ def testAnyDeleg(t, env):
     FLAGS: open deleg all
     CODE: DELEG3
     """
-    recall = threading.Event()
-    def pre_hook(arg, env):
-        recall.stateid = arg.stateid # NOTE this must be done before set()
-        env.notify = recall.set # This is called after compound sent to queue
-    def post_hook(arg, env, res):
-        return res
-    sess1 = env.c1.new_client_session("%s_1" % env.testname(t))
-    sess1.client.cb_pre_hook(OP_CB_RECALL, pre_hook)
-    sess1.client.cb_post_hook(OP_CB_RECALL, post_hook)
-    res = create_file(sess1, env.testname(t),
-                      access=OPEN4_SHARE_ACCESS_READ |
-                      OPEN4_SHARE_ACCESS_WANT_ANY_DELEG)
-    check(res)
-    fh = res.resarray[-1].object
-    deleg = res.resarray[-2].delegation
-    if deleg.delegation_type == OPEN_DELEGATE_NONE or deleg.delegation_type == OPEN_DELEGATE_NONE_EXT:
-        fail("Could not get delegation")
-    sess2 = env.c1.new_client_session("%s_2" % env.testname(t))
-    claim = open_claim4(CLAIM_NULL, env.testname(t))
-    owner = open_owner4(0, "My Open Owner 2")
-    how = openflag4(OPEN4_NOCREATE)
-    open_op = op.open(0, OPEN4_SHARE_ACCESS_BOTH, OPEN4_SHARE_DENY_NONE,
-                      owner, how, claim)
-    slot = sess2.compound_async(env.home + [open_op])
-    # Wait for recall, and return delegation
-    recall.wait() # STUB - deal with timeout
-    # Getting here means CB_RECALL reply is in the send queue.
-    # Give it a moment to actually be sent
-    env.sleep(1)
-    res = sess1.compound([op.putfh(fh), op.delegreturn(recall.stateid)])
-    check(res)
-    # Now get OPEN reply
-    res = sess2.listen(slot)
-    checklist(res, [NFS4_OK, NFS4ERR_DELAY])
+    _testDeleg(t, env, OPEN4_SHARE_ACCESS_READ,
+        OPEN4_SHARE_ACCESS_WANT_ANY_DELEG, OPEN4_SHARE_ACCESS_BOTH);
 
 def testNoDeleg(t, env):
     """Test no delgation handout
