kofemann/pynfs

commit 4b3dcf2bb007e7554df8d02412f9c114980178b6

4.0 server tests: send lots of getattrs in one compound
Send a lot of getattrs in one compound to test new xdr encoding.

This should probably really be a 4.1 test.

Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.0/servertests/st_getattr.py
@@ -482,7 +482,22 @@ def testFSLocations(t, env):
         t.fail_support("fs_locations not a supported attribute")
     # print res.resarray[-1].obj_attributes
 
-    
+def testLotsofGetattrsFile(t, env):
+    """Send lots of getattrs
+
+    FLAGS: getattr file all
+    DEPEND: LOOKFILE
+    CODE: GATT9
+    """
+    c = env.c1
+    mandatory = [attr.bitnum for attr in env.attr_info if attr.mandatory]
+    ops = c.use_obj(env.opts.usefile)
+    for i in range(90):
+        ops += [c.getattr(mandatory)]
+    res = c.compound(ops)
+    check(res)
+
+
 ####################################################
 
     def xxxtestMountedOnFileid(self):

commit f6044647067bbcf8490245b2a873130fae690870

GATT9: ERR_RESOURCE is a reasonable server response
Unfortunately this defeats the original purpose of the test, which is to
check that the server can decode a request greater than a page that
isn't just a WRITE.

Fixes: 4b3dcf2bb007
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.0/servertests/st_getattr.py
@@ -501,7 +501,7 @@ def testLotsofGetattrsFile(t, env):
     for i in range(90):
         ops += [c.getattr(mandatory)]
     res = c.compound(ops)
-    check(res)
+    check(res, [NFS4_OK, NFS4ERR_RESOURCE])
 
 def testOwnerName(t, env):
     """GETATTR on owner