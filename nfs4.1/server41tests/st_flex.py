kofemann/pynfs

commit 0bfa03c2ca2d99b9c1205586551faf3bbf0d1514

st_flex: Fixup check for error in layoutget_return()
Signed-off-by: Tom Haynes <loghyr@hammerspace.com>
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.1/server41tests/st_flex.py
@@ -605,7 +605,7 @@ def layoutget_return(sess, fh, open_stateid, allowed_errors=NFS4_OK,
                         0, NFS4_UINT64_MAX, 4196, open_stateid, 0xffff)]
     res = sess.compound(ops)
     check(res, allowed_errors)
-    if nfsstat4[res.status] is not NFS4_OK:
+    if nfsstat4[res.status] is not 'NFS4_OK':
         return [res] # We can't return the layout without a stateid!
     layout_stateid = res.resarray[-1].logr_stateid
 

commit 8d687906c06632561a99fbd0f32a16b53faa1b20

st_flex: Fix comparison operator, compare int
It fixes Python 3 warning:
    nfs4.1/server41tests/st_flex.py:618: SyntaxWarning: "is not" with a literal. Did you mean "!="?
      if nfsstat4[res.status] is not 'NFS4_OK':

0bfa03c correctly changed NFS4_OK to string, as nfsstat4 dictionary
values are strings, but comparator was not changed.

But instead of just changing operator to '!=' also use res.status
directly thus we can compare with NFS4_OK (int variable) instead of
"NFS4_OK" (string literal => typos not detected).

Fixes: 0bfa03c ("st_flex: Fixup check for error in layoutget_return()")

Signed-off-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.1/server41tests/st_flex.py
@@ -615,7 +615,7 @@ def layoutget_return(sess, fh, open_stateid, allowed_errors=NFS4_OK,
                         0, NFS4_MAXFILELEN, 4196, open_stateid, 0xffff)]
     res = sess.compound(ops)
     check(res, allowed_errors)
-    if nfsstat4[res.status] is not 'NFS4_OK':
+    if res.status != NFS4_OK:
         return [res] # We can't return the layout without a stateid!
     layout_stateid = res.resarray[-1].logr_stateid
 