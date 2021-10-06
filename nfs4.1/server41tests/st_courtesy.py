kofemann/pynfs

commit 18553b92ba8d0e18001f39db77bcabd9138c4808

pynfs: courtesy: add a test to ensure server releases state appropriately
This test checks that the server will allow a conflicting lock from a
second client, after lease expiry of the first client.

This applies to both courteous, and discourteous servers, but ensures
that courteous servers don't just hang onto state indefinitely.

Signed-off-by: Calum Mackay <calum.mackay@oracle.com>
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.1/server41tests/st_courtesy.py
@@ -48,3 +48,36 @@ def testLockSleepLockU(t, env):
 
     res = close_file(sess1, fh, stateid=stateid)
     check(res)
+
+def testLockSleepLock(t, env):
+    """ensure that a courteous server will allow a conflicting lock from
+       a second client, after lease expiry of the first client.
+       A discourteous server should allow this too, of course.
+
+    FLAGS: courteous all
+    CODE: COUR2
+    """
+
+    sess1 = env.c1.new_client_session(env.testname(t))
+
+    res = create_file(sess1, env.testname(t))
+    check(res)
+
+    fh = res.resarray[-1].object
+    stateid = res.resarray[-2].stateid
+    res = sess1.compound(cour_lockargs(fh, stateid))
+    check(res, NFS4_OK)
+
+    lease_time = _getleasetime(sess1)
+    env.sleep(lease_time * 2, "twice the lease period")
+
+    c2 = env.c1.new_client(b"%s_2" % env.testname(t))
+    sess2 = c2.create_session()
+
+    res = open_file(sess2, env.testname(t), access=OPEN4_SHARE_ACCESS_WRITE)
+    check(res)
+
+    fh = res.resarray[-1].object
+    stateid = res.resarray[-2].stateid
+    res = sess2.compound(cour_lockargs(fh, stateid))
+    check(res, NFS4_OK)

commit 58d443bb685c8de2b95bddb05080f6eb81af649a

st_courtesy.py: Fix problem of missing RECLAIM_COMPLETE in COUR2
Replace new_client and create_session with new_client_session.

Fixes: 18553b92ba8d0 ("pynfs: courtesy: add a test to ensure server releases state appropriately")
Signed-off-by: Dai Ngo <dai.ngo@oracle.com>
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.1/server41tests/st_courtesy.py
@@ -69,10 +69,9 @@ def testLockSleepLock(t, env):
     check(res, NFS4_OK)
 
     lease_time = _getleasetime(sess1)
-    env.sleep(lease_time * 2, "twice the lease period")
+    env.sleep(lease_time + 10, "the lease period + 10 secs")
 
-    c2 = env.c1.new_client(b"%s_2" % env.testname(t))
-    sess2 = c2.create_session()
+    sess2 = env.c1.new_client_session(b"%s_2" % env.testname(t))
 
     res = open_file(sess2, env.testname(t), access=OPEN4_SHARE_ACCESS_WRITE)
     check(res)