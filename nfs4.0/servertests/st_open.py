kofemann/pynfs

commit f8740be0ffea4883ab229f0766cd57add2523150

4.0 server tests: OPEN of special file should return SYMLINK
See http://marc.info/?l=linux-nfs&m=131342421825436&w=2 "RE: open() of
device special files" and followups for discussion; NFS4ERR_SYMLINK will
trigger the correct behavior on the Linux client, at least.

Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.0/servertests/st_open.py
@@ -156,7 +156,7 @@ def testBlock(t, env):
     c = env.c1
     c.init_connection()
     res = c.open_file(t.code, env.opts.useblock)
-    check(res, NFS4ERR_INVAL, "Trying to OPEN block device")
+    check(res, NFS4ERR_SYMLINK, "Trying to OPEN block device")
 
 def testChar(t, env):
     """OPEN with a character device should return NFS4ERR_INVAL
@@ -168,7 +168,7 @@ def testChar(t, env):
     c = env.c1
     c.init_connection()
     res = c.open_file(t.code, env.opts.usechar)
-    check(res, NFS4ERR_INVAL, "Trying to OPEN character device")
+    check(res, NFS4ERR_SYMLINK, "Trying to OPEN character device")
 
 def testSocket(t, env):
     """OPEN with a socket should return NFS4ERR_INVAL
@@ -180,7 +180,7 @@ def testSocket(t, env):
     c = env.c1
     c.init_connection()
     res = c.open_file(t.code, env.opts.usesocket)
-    check(res, NFS4ERR_INVAL, "Trying to OPEN socket")
+    check(res, NFS4ERR_SYMLINK, "Trying to OPEN socket")
 
 def testFifo(t, env):
     """OPEN with a fifo should return NFS4ERR_INVAL
@@ -192,7 +192,7 @@ def testFifo(t, env):
     c = env.c1
     c.init_connection()
     res = c.open_file(t.code, env.opts.usefifo)
-    check(res, NFS4ERR_INVAL, "Trying to OPEN fifo")
+    check(res, NFS4ERR_SYMLINK, "Trying to OPEN fifo")
 
 def testNoFh(t, env):
     """OPEN should fail with NFS4ERR_NOFILEHANDLE if no (cfh)

commit 925fae8cbdc1eef706621857db65207ad3738757

4.0 open tests: Update comments with incorrect error codes
Fixes: f8740be0ffea "4.0 server tests: OPEN of special file should..."
Reported-by: Manohar Thorat <manoharthorat725@gmail.com>
Signed-off-by: J. Bruce Fields <bfields@redhat.com>

nfs4.0/servertests/st_open.py
@@ -147,7 +147,7 @@ def testLink(t, env):
     check(res, NFS4ERR_SYMLINK, "Trying to OPEN symbolic link")
     
 def testBlock(t, env):
-    """OPEN with a block device should return NFS4ERR_INVAL
+    """OPEN with a block device should return NFS4ERR_SYMLINK
 
     FLAGS: open block all
     DEPEND: INIT LOOKBLK
@@ -159,7 +159,7 @@ def testBlock(t, env):
     check(res, NFS4ERR_SYMLINK, "Trying to OPEN block device")
 
 def testChar(t, env):
-    """OPEN with a character device should return NFS4ERR_INVAL
+    """OPEN with a character device should return NFS4ERR_SYMLINK
 
     FLAGS: open char all
     DEPEND: INIT LOOKCHAR
@@ -171,7 +171,7 @@ def testChar(t, env):
     check(res, NFS4ERR_SYMLINK, "Trying to OPEN character device")
 
 def testSocket(t, env):
-    """OPEN with a socket should return NFS4ERR_INVAL
+    """OPEN with a socket should return NFS4ERR_SYMLINK
 
     FLAGS: open socket all
     DEPEND: INIT LOOKSOCK
@@ -183,7 +183,7 @@ def testSocket(t, env):
     check(res, NFS4ERR_SYMLINK, "Trying to OPEN socket")
 
 def testFifo(t, env):
-    """OPEN with a fifo should return NFS4ERR_INVAL
+    """OPEN with a fifo should return NFS4ERR_SYMLINK
 
     FLAGS: open fifo all
     DEPEND: INIT LOOKFIFO